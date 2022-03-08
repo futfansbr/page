02e98cd7bf6d69c3372184a9363e7833269bc54b
futfansbr <100816422+futfansbr@users.noreply.github.com>
2022-03-08 02:53:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-const details=document.querySelectorAll("details");function Rastrear(){var x=document.getElementById("active-input").value;if(13===x.length||16===x.length){var site,format="https://www.muambator.com.br/pacotes/"+x+"/detalhes/";window.open(format,"_blank")}else document.getElementById("active-input").value="",document.getElementById("active-input").placeholder="Este código é inválido!"}function Botao(x){x.setAttribute("style","display:none"),document.getElementById("active-block").setAttribute("style","display:block"),document.getElementById("active-block").setAttribute("style","border-color:#f0e800"),document.getElementById("active-input").select(),document.getElementById("active-button").setAttribute("style","display:block")}details.forEach(targetDetail=>{targetDetail.addEventListener("click",()=>{details.forEach(detail=>{detail!==targetDetail&&detail.removeAttribute("open")})})});
+    const details = document.querySelectorAll("details");
+    details.forEach((targetDetail) => {
+    targetDetail.addEventListener("click", () => {
+        details.forEach((detail) => {
+        if (detail !== targetDetail) {
+            detail.removeAttribute("open");
+        }
+    });
+});
+});
+
+function Rastrear(){
+    var x = document.getElementById("active-input").value;
+    if (x.length === 13 || x.length === 16) {
+    var site = "https://www.muambator.com.br/pacotes/"
+    var format = site + x + "/detalhes/";
+    window.open(format, '_blank');
+    } else {
+    document.getElementById("active-input").value = "";
+
+    document.getElementById("active-input").placeholder = "Este código é inválido!";  
+}
+}
+
+function Botao(x){
+    x.setAttribute('style', 'display:none');
+    document.getElementById("active-block").setAttribute('style', 'display:block');
+    document.getElementById("active-block").setAttribute('style', 'border-color:#f0e800');
+    document.getElementById("active-input").select();
+    document.getElementById("active-button").setAttribute('style', 'display:block');
+}
